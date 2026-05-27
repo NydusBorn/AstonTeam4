e5ad819bc73c3debbdbf3f556234114c50691eb2
Артём Евсеев <artm.yevseev@list.ru>
2026-05-27 18:29:08 +0300

    Update InsertionSort.java

---
+++ src/main/java/InsertionSort.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public static void insertionSort(List<Integer> list) {
+    for (int i = 1; i < list.size(); i++) {
+        int key = list.get(i);
+        int j = i - 1;
+        while (j >= 0 && list.get(j) > key) {
+            list.set(j + 1, list.get(j));
+            j--;
+        }
+        list.set(j + 1, key);
+    }
+}
